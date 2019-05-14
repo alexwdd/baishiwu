a4441fe11cf3dc7b017835d4e394e2f882669e29
alexwdd <20779512@qq.com>
2019-05-14 13:26:37 +0800

    支付宝支付

--- app/V1/Controller/ChongzhiController.class.php
+++ app/V1/Controller/ChongzhiController.class.php
@@ -185,13 +185,49 @@ class ChongzhiController extends CommonController {
 	    		if ($data['payType']==2) {
 	    			$result = $this->wxPub($data);
 	    		}else{
-	    			returnJson('-1','暂不⽀持⽀付宝支付');
+	    			$result = $this->aliPub($data);
 	    		}    		
 	    		returnJson(0,'success',$result);
 	    	}else{
 	    		returnJson('-1','操作失败');
 	    	}
     	}
+    }
+
+    //支付宝统一下单
+    public function aliPub($order){
+    	$config = C("XINJIAPO_PAY");
+
+    	vendor('appalipay.AopSdk');// 加载类库
+
+    	$aop = new \AopClient;
+		$aop->gatewayUrl = "https://openapi.alipay.com/gateway.do";
+		$aop->appId = $config['ALI_APP_ID'];
+		$aop->rsaPrivateKey = $config['ALI_PRI_KEY'];
+		$aop->format = "json";
+		$aop->charset = "UTF-8";
+		$aop->signType = "RSA2";
+		$aop->alipayrsaPublicKey = $config['ALI_PUB_KEY'];
+		//实例化具体API对应的request类,类名称和接口名称对应,当前调用接口名称：alipay.trade.app.pay
+		$request = new \AlipayTradeAppPayRequest();
+		//SDK已经封装掉了公共参数，这里只需要传入业务参数
+		$content = array();
+		$content['body'] = '在线支付';
+		$content['subject'] = '手机充值';//商品的标题/交易标题/订单标题/订单关键字等
+		$content['out_trade_no'] = $order['order_no'];//商户网站唯一订单号
+		$content['timeout_express'] = '1d';//该笔订单允许的最晚付款时间
+		$content['total_amount'] = floatval($order['rmb']);//订单总金额(必须定义成浮点型)
+		#$content['total_amount'] = 0.01;//订单总金额(必须定义成浮点型)
+		$content['product_code'] = 'QUICK_MSECURITY_PAY';//
+
+		$bizcontent = json_encode($content);
+		$request->setNotifyUrl(C('site.domain').'/v1/chongzhi/alinotify/');
+		$request->setBizContent($bizcontent);
+		//这里和普通的接口调用不同，使用的是sdkExecute
+		$response = $aop->sdkExecute($request);
+		//htmlspecialchars是为了输出到页面时防止被浏览器将关键参数html转义，实际打印到日志以及http传输不会有这个问题
+		$str = $response;//就是orderString 可以直接给客户端请求，无需再做处理。
+		return array('url'=>$str,'order_no'=>$order['order_no']);
     }
 
     //微信下单
@@ -218,6 +254,49 @@ class ChongzhiController extends CommonController {
         $jsApiParameters = $jsApi->getAppParameters();
         $jsApiParameters['order_no'] = $order['order_no'];
         return $jsApiParameters;
+    }
+
+    public function alinotify(){
+    	$config = C("XINJIAPO_PAY");
+    	vendor('appalipay.AopSdk');// 加载类库
+        $aop = new \AopClient;
+        $aop->alipayrsaPublicKey = $config['ALI_PUB_KEY'];
+
+        //file_put_contents("log".date("Y-m-d",time()).".txt", date ( "Y-m-d H:i:s" ) . "  "."订单" .$_POST['out_trade_no']. "\r\n", FILE_APPEND);
+
+        $flag = $aop->rsaCheckV1($_POST, NULL, "RSA2");
+        if($flag){
+            //商户订单号
+            $out_trade_no = $_POST['out_trade_no'];
+            //支付宝交易号
+            $trade_no = $_POST['trade_no'];
+            //交易状态
+            $trade_status = $_POST['trade_status'];
+            //订单的实际金额
+            $total_amount = $_POST['total_amount'];
+            //appid
+            $appid = $_POST['app_id'];
+            $seller_id = $_POST['seller_id'];
+            //验证app_id是否为商户本身
+            if($appid != $config['ALI_APP_ID']){
+                exit('fail');
+            }
+            //判断交易通知状态是否为TRADE_SUCCESS或TRADE_FINISH
+            if($trade_status!='TRADE_FINISH' && $trade_status !='TRADE_SUCCESS'){
+                exit('fail');
+            }            
+
+            //验证订单的准确性
+            if(!empty($out_trade_no)){
+                $data['order_no'] = $out_trade_no;
+    			$url = C('site.domain').'/v1/chongzhi/changeOrderStatus/';
+    			$res = $this->https_post($url,$data);
+    			//file_put_contents("log".date("Y-m-d",time()).".txt", date ( "Y-m-d H:i:s" ) . "  "."订单" .$out_trade_no. "，".$url."\r\n", FILE_APPEND);
+            }
+            echo 'success';
+        } else {
+            echo 'fail';
+        }
     }
 
     public function wxnotify(){    	

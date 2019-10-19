89250851247dd7f4f43c9d38ccf660c47cd4bb4b
alexwdd <alex_wdd@sina.com.cn>
2019-10-19 15:59:07 +0800

    1019

--- app/Daigou/Controller/AgsController.class.php
+++ app/Daigou/Controller/AgsController.class.php
@@ -14,4 +14,46 @@ class AgsController extends BaseController {
         $this->display();
     }
 
+    public function mprint(){
+		$ids = I('get.ids');
+		$ids = explode(",",$ids);
+
+		$map['eimg'] = array('neq','');
+		$map['id'] = array('in',$ids);
+		M("DgOrderBaoguo")->where($map)->setField('print',1);
+
+		$list = M("DgOrderBaoguo")->where($map)->select();
+		$this->assign('list',$list);
+
+		unset($map);
+		$map['id'] = array('in',$ids);
+		$map['eimg'] = array('neq','');
+		$map['type'] = array('in',[1,2,3]);
+		$map['sign'] = array('eq','');
+		M("DgOrderBaoguo")->where($map)->save(['flag'=>1,'updateTime'=>time()]);
+
+
+		foreach ($list as $key => $value) {
+			unset($where);
+			$where['orderID'] = $value['orderID'];
+        	$where['print'] = 0;
+        	$printNumber = M("DgOrderBaoguo")->where($where)->count();//未打印总数
+
+        	unset($where);
+			$where['orderID'] = $value['orderID'];
+        	$where['flag'] = 0;
+        	$flagNumber = M("DgOrderBaoguo")->where($where)->count();//未发货总数
+
+
+        	unset($map);
+    		$map['id'] = $value['orderID'];
+    		$map['payStatus'] = array('in',[2,3]);
+        	if ($flagNumber==0 && $printNumber==0) {
+        		M("DgOrder")->where($map)->setField("payStatus",4);
+        	}elseif($printNumber==0){
+	        	M("DgOrder")->where($map)->setField("payStatus",3);
+        	}
+		}
+		$this->display();
+	}
 }

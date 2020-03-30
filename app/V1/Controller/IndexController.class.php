d4d09696574e1132d6290010db7bb77a6b663364
alexwdd <alex_wdd@sina.com.cn>
2020-03-30 23:22:02 +0800

    0330

---
+++ app/V1/Controller/IndexController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace V1\Controller;
+use Common\Controller\BaseController;
+
+class IndexController extends BaseController {
+
+	public function index() {
+        echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title><script data-ad-client="ca-pub-7660344576875968" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></head><body></body></html>';
+	} 
+}

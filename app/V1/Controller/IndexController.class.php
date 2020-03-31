dfa2581dc2b3e69df5ebf9f1095077a2e7f0e8d7
alexwdd <20779512@qq.com>
2020-03-31 16:14:46 +0800

    0331

--- app/V1/Controller/IndexController.class.php
+++ app/V1/Controller/IndexController.class.php
@@ -5,6 +5,6 @@ use Common\Controller\BaseController;
 class IndexController extends BaseController {
 
 	public function index() {
-        echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title><script data-ad-client="ca-pub-7660344576875968" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></head><body></body></html>';
+        $this->display();
 	} 
 }
